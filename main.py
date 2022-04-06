b29b594347e4186bd9f0cbbbbfb910e65bfc3909
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-06 16:43:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Body Mass Index Calculator
+height = input("What is your height in m: ")
+weight = input("What is your current weight in kg: ")
+
+total = weight / height**
+print(body_mass_index)
