04dd414fc288a0deabc284f56c0fcb4cf4839c61
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-06 21:33:18 +0000

    Figured out how to turn a number into a float and rounding it.

--- main.py
+++ main.py
@@ -2,5 +2,8 @@
 height = input("What is your height in m: ")
 weight = input("What is your current weight in kg: ")
 
-total = weight / height**
-print(body_mass_index)
+int_height = float(height)
+int_weight = int(weight)
+total = int_weight / int_height ** int_height
+print("Your current BMI is: " + str(round(total)))
+
